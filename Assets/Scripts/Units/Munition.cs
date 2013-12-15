3ad3552bb2934cfa5b6a66c8ba039ae9436b442a
William Vickery <william@vickery.com>
2013-12-15 02:29:36 -0600

    wokring on units attacking other

--- Assets/Scripts/Units/Munition.cs
+++ Assets/Scripts/Units/Munition.cs
@@ -9,16 +9,35 @@ public class Munition : MonoBehaviour {
     public Unit.UnitType targetType = Unit.UnitType.Air;
     public float damage = 10; // 10 damage
 
+    private float z = 1.35f;
+
     //Use this for initialization
-    void Start()
+    void Awake()
     {
-        m_direction.x = 1;
-        m_direction.y = 0;
+        Rigidbody2D rigidBody2D = gameObject.AddComponent("Rigidbody2D") as Rigidbody2D;
+        rigidBody2D.isKinematic = true;
+        rigidBody2D.fixedAngle = true;
+        rigidBody2D.gravityScale = 0f;
+        //m_direction.x = 1;
+        //m_direction.y = 0;
 	}
 	
     //This function is called every fixed framerate frame
 	void FixedUpdate () {
-	    float newX = velocity * m_direction.x * Time.deltaTime;
-        float newY = velocity * m_direction.y * Time.deltaTime;
-	}
+        //Vector2 currentPosition = transform.position;
+        
+
+        //float newX = (velocity * m_direction.x * Time.deltaTime) + currentPosition.x;
+        //float newY = (velocity * m_direction.y * Time.deltaTime) + currentPosition.y;
+
+
+        //Vector3 newPosition = new Vector3(newX, newY, z);
+
+        //transform.position = newPosition;
+    }
+
+    public void FireAt(Vector2 position, Vector2 targetDirection) {
+        transform.position = new Vector3(position.x, position.y, 1.35f);
+        gameObject.GetComponent<Rigidbody2D>().velocity = targetDirection * velocity;
+    }
 }
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -5,27 +5,57 @@ public class Unit : MonoBehaviour {
 
 
     private Point m_source, m_destination;
+    private Unit m_target;
 
     public enum UnitType { Ground, Air };
 
     public float velocity = 100; // pixels per second
     private Vector2 m_direction;
 
-    private SphereCollider m_range;
+    private GameObject m_range;
     public float range = 10;
+
+    public GameObject munition;
 
     public int health = 100;
 
     public UnitType type = UnitType.Air;
 
+    public int shootFrame, shootCount;
+
     //Use this for initialization
-    void Start() {
-        m_direction.x = 1;
+    void Start()
+    {
+        m_direction.x = 0;
         m_direction.y = 0;
 
-        m_range = gameObject.AddComponent("SphereCollider") as SphereCollider;
-        m_range.radius = range;
-        m_range.isTrigger = true;
+        shootFrame = 0;
+        shootCount = 20;
+
+        InitBody();
+        InitRange();
+    }
+
+    void InitBody(){
+        Rigidbody2D rigidBody2D = gameObject.AddComponent("Rigidbody2D") as Rigidbody2D;
+        rigidBody2D.isKinematic = false;
+        rigidBody2D.fixedAngle = true;
+        rigidBody2D.gravityScale = 0f;
+
+
+        BoxCollider2D colliderTemp = gameObject.AddComponent<BoxCollider2D>();
+        colliderTemp.size = gameObject.GetComponent<tk2dSprite>().GetBounds().size;
+        colliderTemp.isTrigger = true;
+    }
+
+    void InitRange(){
+        //m_range = new GameObject();
+        //m_range.name = "UnitRange";
+        //m_range.transform.position = transform.position;
+        //m_range.transform.parent = transform;
+        CircleCollider2D colliderTemp = gameObject.AddComponent<CircleCollider2D>();
+        colliderTemp.radius = range;
+        colliderTemp.isTrigger = true;
     }
 	
 	// Update is called once per frame
@@ -37,6 +67,7 @@ public class Unit : MonoBehaviour {
         // at some point assign the target point and make it point in that direction
         m_destination = target;
         m_direction = new Vector2(m_destination.position.x - transform.position.x, m_destination.position.y - transform.position.y);
+        m_direction.Normalize();
     }
 
     void Source(Point source) {
@@ -53,6 +84,24 @@ public class Unit : MonoBehaviour {
         Vector2 newPosition = new Vector2(newX, newY);
 
         transform.position = newPosition;
+
+        if (m_target)
+        {
+            if (shootFrame == 0)
+            {
+                Attack(m_target);
+                shootFrame++;
+            }
+            else
+            {
+                shootFrame++;
+                if (shootFrame == shootCount)
+                {
+                    shootFrame = 0;
+                }
+            }
+
+        }
     }
     
     //returns the current position of the Unit
@@ -65,12 +114,31 @@ public class Unit : MonoBehaviour {
 
     //overidable method used to attack an object
     void Attack(Unit target){
-       //fire current unity munition at target
-      
+        //fire current unity munition at target
+        //GameObject payloadGameObject = ((GameObject)Instantiate(munition));
+        //Munition payloadMunition = payloadGameObject.GetComponent<Munition>();
+
+        //payloadMunition.gameObject.transform.position = transform.position;
+
+        //Vector2 fireDirection = target.GetPosition() - GetPosition();
+        //fireDirection.Normalize();
+
+        //payloadMunition.FireAt(transform.position, fireDirection);
     }
 
     void TakeDamage(Munition bullet){
         
     }
+
+    //OnTriggerEnter is called when the Collider other enters the trigger.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered by " + other.GetType());
+        if (other is BoxCollider2D) {
+            m_target = other.gameObject.GetComponent<Unit>();
+        }
+
+    }
+
    
 }
