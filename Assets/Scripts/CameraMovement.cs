48a3cf8de63693b9b2f8799778f6e4e39fad7c72
Darwin <darwinpek@gmail.com>
2013-12-14 22:29:39 -0600

    Added bounds to camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,9 @@ public class CameraMovement : MonoBehaviour {
 	
 	public Vector2 m_moveSpeed = new Vector2(.5f,.5f);
 	
+	public Vector2 m_minBounds;
+	public Vector2 m_maxBounds;
+	
 	// Update is called once per frame
 	void Update () {
 		Vector2 delta = m_moveSpeed;
@@ -12,6 +15,18 @@ public class CameraMovement : MonoBehaviour {
 		delta.y *= Input.GetAxis("Vertical");
 		
 		Vector3 newPos = (transform.position + (Vector3)delta);
+		if(newPos.x > m_maxBounds.x) {
+			newPos.x = m_maxBounds.x;
+		} else if(newPos.x < m_minBounds.x) {
+			newPos.x = m_minBounds.x;
+		}
+		
+		if(newPos.y > m_maxBounds.y) {
+			newPos.y = m_maxBounds.y;
+		} else if(newPos.y < m_minBounds.y) {
+			newPos.y = m_minBounds.y;
+		}
+		
 		transform.position = newPos;
 	}
 }
