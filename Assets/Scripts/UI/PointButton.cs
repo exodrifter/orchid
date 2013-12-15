517d374fd5a902a1166b8f689eb6a99e72c011a7
Darwin <darwinpek@gmail.com>
2013-12-14 18:08:23 -0600

    Initial work on Point gameobject

---
+++ Assets/Scripts/UI/PointButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PointButton : MonoBehaviour {
+	
+	void Update () {
+		
+	}
+	
+	void OnMouseOver() {
+		Debug.Log("Hello");
+	}
+}
