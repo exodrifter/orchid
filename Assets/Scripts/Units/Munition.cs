7e66b82973b14b0284ecd80b42a16a618657ba8a
William Vickery <william@vickery.com>
2013-12-14 20:08:39 -0600

    Initial implementation of Unit base class and Muntion bass class

---
+++ Assets/Scripts/Units/Munition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Munition : MonoBehaviour {
+
+    public float velocity = 100; // pixels per second
+    private Vector2 m_direction;
+    
+    public Unit.UnitType targetType = Unit.UnitType.Air;
+    public float damage = 10; // 10 damage
+
+    //Use this for initialization
+    void Start()
+    {
+        m_direction.x = 1;
+        m_direction.y = 0;
+	}
+	
+    //This function is called every fixed framerate frame
+	void FixedUpdate () {
+	    float newX = velocity * m_direction.x * Time.deltaTime;
+        float newY = velocity * m_direction.y * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+    //TODO need to get these from darwin
+        //SourcePoint
+        //DestinationPoint
+
+    public enum UnitType { Ground, Air };
+
+    public float velocity = 100; // pixels per second
+    private Vector2 m_direction;
+
+    public int health = 100;
+
+    public UnitType type = UnitType.Air;
+
+    //Use this for initialization
+    void Start()
+    {
+        m_direction.x = 1;
+        m_direction.y = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Target() {
+        // at some point assign the target point and make it point in that direction
+    }
+
+    //This function is called every fixed framerate frame
+    void FixedUpdate(){
+        Vector2 currentPosition = GetPosition();
+
+        float newX = (velocity * m_direction.x * Time.deltaTime) + currentPosition.x;
+        float newY = (velocity * m_direction.y * Time.deltaTime) + currentPosition.y;
+
+        Vector2 newPosition = new Vector2(newX, newY);
+
+        transform.position = newPosition;
+    }
+    
+    //returns the current position of the Unit
+    Vector2 GetPosition(){
+        float myX = transform.position.x;
+        float myY = transform.position.y;
+        
+        return new Vector2(myX, myY);
+    }
+
+    //overidable method used to attack an object
+    void Attack(Unit target){
+       //fire current unity munition at target
+      
+    }
+
+    void TakeDamage(Munition bullet){
+        
+    }
+   
+}
