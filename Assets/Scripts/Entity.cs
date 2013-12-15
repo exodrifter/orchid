3ee0bd1a7edb5bb12c341dc479fda552d2aa9dfc
William Vickery <william@vickery.com>
2013-12-15 14:15:23 -0600

    started entity refactor

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+    public enum Type {bomber, fighter, icbm, point};
+
+	/// <summary>
+	/// The owner of this point
+	/// </summary>
+	public Owner m_owner = Owner.ENEMY;
+	
+	/// <summary>
+	/// The amount of HP that this point has
+	/// </summary>
+	protected int m_hp;
+	
+	/// <summary>
+	/// The amount of damage that this point has recieved
+	/// </summary>
+	protected int m_damage;
+	
+	public int hp {
+		get { return m_hp; }
+	}
+	
+	public int damage {
+		get { return m_damage; }
+		set { m_damage = value; }
+	}
+	
+	public bool dead {
+		get { return m_damage >= m_hp; }
+	}
+	
+    public Vector2 position {
+        get { return transform.position; }
+    }
+}
--- Assets/Scripts/Point/Point.cs
+++ Assets/Scripts/Point/Point.cs
@@ -5,46 +5,15 @@ using System.Collections;
 /// Generic point class for all of the locations on the map that the player can
 /// interact with.
 /// </summary>
-public abstract class Point : MonoBehaviour {
-	
-	/// <summary>
-	/// The owner of this point
-	/// </summary>
-	public Owner m_owner = Owner.ENEMY;
-	
-	/// <summary>
-	/// The amount of HP that this point has
-	/// </summary>
-	protected int m_hp;
-	
-	/// <summary>
-	/// The amount of damage that this point has recieved
-	/// </summary>
-	protected int m_damage;
-	
+public abstract class Point : Entity {
+		
 	/// <summary>
 	/// The amount of money that this point should reward the attacker when destroyed
 	/// </summary>
 	protected int m_money;
 	
-	public int hp {
-		get { return m_hp; }
-	}
-	
-	public int damage {
-		get { return m_damage; }
-		set { m_damage = value; }
-	}
-	
-	public bool dead {
-		get { return m_damage >= m_hp; }
-	}
-	
 	public int money {
 		get { return m_money; }
 	}
 
-    public Vector2 position {
-        get { return transform.position; }
-    }
 }
