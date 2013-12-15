f3bf3f4262189dd08a4e408cafc5cb564aad9559
William Vickery <william@vickery.com>
2013-12-15 03:58:53 -0600

    major overhaul of base unit and munition

--- Assets/Scripts/Units/Munition.cs
+++ Assets/Scripts/Units/Munition.cs
@@ -3,41 +3,12 @@ using System.Collections;
 
 public class Munition : MonoBehaviour {
 
-    public float velocity = 100; // pixels per second
-    private Vector2 m_direction;
-    
     public Unit.UnitType targetType = Unit.UnitType.Air;
-    public float damage = 10; // 10 damage
 
-    private float z = 1.35f;
+    public Color colour = Color.red;
+    
+    public float damage = 10.0f;
 
-    //Use this for initialization
-    void Awake()
-    {
-        Rigidbody2D rigidBody2D = gameObject.AddComponent("Rigidbody2D") as Rigidbody2D;
-        rigidBody2D.isKinematic = true;
-        rigidBody2D.fixedAngle = true;
-        rigidBody2D.gravityScale = 0f;
-        //m_direction.x = 1;
-        //m_direction.y = 0;
-	}
-	
-    //This function is called every fixed framerate frame
-	void FixedUpdate () {
-        //Vector2 currentPosition = transform.position;
-        
-
-        //float newX = (velocity * m_direction.x * Time.deltaTime) + currentPosition.x;
-        //float newY = (velocity * m_direction.y * Time.deltaTime) + currentPosition.y;
-
-
-        //Vector3 newPosition = new Vector3(newX, newY, z);
-
-        //transform.position = newPosition;
-    }
-
-    public void FireAt(Vector2 position, Vector2 targetDirection) {
-        transform.position = new Vector3(position.x, position.y, 1.35f);
-        gameObject.GetComponent<Rigidbody2D>().velocity = targetDirection * velocity;
-    }
+    public float attack_time = 1.0f;
+    public float attack_time_variance = 1.0f;
 }
